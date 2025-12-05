22c1734ea3fedcb3425f6812dcdcdbd1356bdfeb
Sachin Singh <sachinsingh19012@gmail.com>
2025-12-05 19:01:10 +0530

    practice question of sets

---
+++ 01_problem.py
@@ -0,0 +1,3 @@
+# Create a set with three items and print it.
+thisset={5,4,6}
+print(thisset)
---
+++ 02_problem.py
@@ -0,0 +1,3 @@
+# Create a set that contains mixed data types like string, int, float. Print the set.
+thisset={"sachin",4,5.6,True}
+print(thisset)
