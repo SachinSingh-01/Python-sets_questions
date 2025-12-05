453986c963bf481556c4fe5436d0222121c82d41
Sachin Singh <sachinsingh19012@gmail.com>
2025-12-05 19:19:31 +0530

    practice question of sets

---
+++ 04_problem.py
@@ -0,0 +1,3 @@
+# Create a set with values: {True, 1, False, 0}. Print the set and explain why values are removed.
+myset={True, 1, False, 0}
+print(myset) #here True and 1 or False and 0 are considered as same value that's why only True and False are print in output
